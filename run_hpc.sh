b8cda23454b2b7a014e5d9a303d6cdd93ff5c025
Joost de Folter <folterj@gmail.com>
2024-09-13 14:04:54 +0200

    Modified hpc script

--- run_hpc.sh
+++ run_hpc.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=64G   # Memory pool for all cores (see also --mem-per-cpu)
 
 export PYTHONUNBUFFERED=TRUE
-#ml purge
-#ml Anaconda3
+ml purge
+ml Anaconda3
 source /camp/apps/eb/software/Anaconda/conda.env.sh
 conda activate multiview-stitcher-cont-env
 python run.py
