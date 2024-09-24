3e4ae40dc6fb88c8fdf7e74c0d5a2ab0f9904dfa
Joost de Folter <folterj@gmail.com>
2024-09-24 12:50:37 +0200

    Update run_hpc.sh

--- run_hpc.sh
+++ run_hpc.sh
@@ -9,5 +9,5 @@ export PYTHONUNBUFFERED=TRUE
 ml purge
 ml Anaconda3
 source /camp/apps/eb/software/Anaconda/conda.env.sh
-conda activate multiview-stitcher-cont-env
+conda activate multiview-stitcher-env
 python run.py
