f2e664230e30da22e7133a383a2ff5c266342748
Joost de Folter <folterj@gmail.com>
2025-01-31 15:03:39 +0100

    Create run_hpc_EM04654.sh

---
+++ run_hpc_EM04654.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=multiview_stitcher_test
+#SBATCH --part=ncpu
+#SBATCH --cpus-per-task=16
+#SBATCH --time=240          # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH --mem=64G   # Memory pool for all cores (see also --mem-per-cpu)
+
+export PYTHONUNBUFFERED=TRUE
+ml purge
+ml Anaconda3
+source /camp/apps/eb/software/Anaconda/conda.env.sh
+conda activate multiview-stitcher-source-env
+python run.py --params=resources/params_EM04654_slice011.yml
