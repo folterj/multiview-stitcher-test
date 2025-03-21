Fixed orientation normalisation and overlap validation, better check existing output, improved summary stats

src/MVSRegistration.py
@@ -15,7 +15,7 @@ import xarray as xr
 
 from src.Video import Video
 from src.image.flatfield import flatfield_correction
-from src.image.ome_helper import save_image
+from src.image.ome_helper import save_image, exists_output
 from src.image.ome_tiff_helper import save_tiff
 from src.image.source_helper import create_source
 from src.image.util import *
@@ -75,9 +75,11 @@ class MVSRegistration:
         parts = split_underscore_numeric(filenames[0])
         output_pattern = params['output'].format_map(parts)
         output = os.path.join(input_dir, output_pattern)    # preserve trailing slash: do not use os.path.normpath()
+        registered_fused_filename = output + 'registered'
+
         output_dir = os.path.dirname(output)
-        if not overwrite and (os.path.exists(output_dir) and os.listdir(output_dir)):
-            logging.warning(f'Skipping non-empty output directory {os.path.normpath(output_dir)}')
+        if not overwrite and exists_output(registered_fused_filename):
+            logging.warning(f'Skipping existing output {os.path.normpath(output_dir)}')
             return
         if clear:
             shutil.rmtree(output_dir, ignore_errors=True)
@@ -101,14 +103,13 @@ class MVSRegistration:
                 if self.verbose:
                     print('\t'.join(map(str, row)))
 
-        registered_fused_filename = output + 'registered'
         if len(filenames) == 1:
             logging.warning('Skipping registration (single image)')
             save_image(registered_fused_filename, sims[0], channels=channels, translation0=positions[0],
                        params=output_params)
             return
 
-        overlaps = self.validate_overlap(sims, file_labels, is_stack or is_channel_overlay)
+        overlaps = self.validate_overlap(sims, file_labels, is_stack, is_stack or is_channel_overlay)
         overall_overlap = np.mean(overlaps)
         if overall_overlap < overlap_threshold:
             raise ValueError(f'Not enough overlap: {overall_overlap * 100:.1f}%')
@@ -133,6 +134,7 @@ class MVSRegistration:
                        params=output_params)
 
         results = self.register(sims, params)
+
         reg_result = results['reg_result']
         sims = results['sims']
         mappings = results['mappings']
@@ -173,12 +175,6 @@ class MVSRegistration:
                 if self.verbose:
                     print('\t'.join(map(str, row)))
 
-        fused_image = self.fuse(sims, mappings, params)
-        logging.info('Saving fused image...')
-        save_image(registered_fused_filename, fused_image,
-                   transform_key=self.reg_transform_key, channels=channels, translation0=positions[0],
-                   params=output_params)
-
         # plot the image configuration after registration
         logging.info('Plotting images...')
         registered_positions_filename = output + 'positions_registered.pdf'
@@ -197,6 +193,12 @@ class MVSRegistration:
             figure, axes = summary_plot
             summary_plot_filename = output + 'groupwise_resolution.pdf'
             figure.savefig(summary_plot_filename)
+
+        fused_image = self.fuse(sims, mappings, params)
+        logging.info('Saving fused image...')
+        save_image(registered_fused_filename, fused_image,
+                   transform_key=self.reg_transform_key, channels=channels, translation0=positions[0],
+                   params=output_params)
 
         if is_transition:
             logging.info('Creating transition...')
@@ -322,19 +324,31 @@ class MVSRegistration:
             sims.append(sim.chunk({'y': 1024, 'x': 1024}))
         return sims, translations, rotations
 
-    def validate_overlap(self, sims, labels, expect_large_overlap=False):
+    def validate_overlap(self, sims, labels, is_stack=False, expect_large_overlap=False):
         overlaps = []
         n = len(sims)
         positions = [si_utils.get_origin_from_sim(sim, asarray=True) for sim in sims]
         sizes = [np.linalg.norm(get_sim_physical_size(sim)) for sim in sims]
+        if self.verbose:
+            print('Overlap:')
         for i in range(n):
             norm_dists = []
-            for j in range(i + 1, n):
-                distance = math.dist(positions[i], positions[j])
-                norm_dist = distance / np.mean([sizes[i], sizes[j]])
-                norm_dists.append(norm_dist)
+            if is_stack:
+                if i + 1 < n:
+                    compare_indices = [i + 1]
+                else:
+                    compare_indices = []
+            else:
+                compare_indices = range(n)
+            for j in compare_indices:
+                if not j == i:
+                    distance = math.dist(positions[i], positions[j])
+                    norm_dist = distance / np.mean([sizes[i], sizes[j]])
+                    norm_dists.append(norm_dist)
             if len(norm_dists) > 0:
                 norm_dist = min(norm_dists)
+                if self.verbose:
+                    print(f'{labels[i]} norm distance: {norm_dist:.3f}')
                 if norm_dist >= 1:
                     logging.warning(f'{labels[i]} has no overlap')
                     overlaps.append(False)
@@ -519,6 +533,13 @@ class MVSRegistration:
             progress.update()
             progress.close()
 
+        if is_stack:
+            # set 3D affine transforms from 2D registration params
+            for index, sim in enumerate(sims):
+                affine_3d = param_utils.identity_transform(ndim=3)
+                affine_3d.loc[{dim: mappings[index].coords[dim] for dim in mappings[index].sel(t=0).dims}] = mappings[index].sel(t=0)
+                si_utils.set_sim_affine(sim, affine_3d, transform_key=self.reg_transform_key, base_transform_key=self.source_transform_key)
+
         return {'reg_result': reg_result,
                 'mappings': mappings_dict,
                 'residual_errors': residual_error_dict,
@@ -542,12 +563,6 @@ class MVSRegistration:
                 output_chunksize[dim] = 1
 
         if is_stack:
-            # set 3D affine transforms from 2D registration params
-            for index, sim in enumerate(sims):
-                affine_3d = param_utils.identity_transform(ndim=3)
-                affine_3d.loc[{dim: mappings[index].coords[dim] for dim in mappings[index].sel(t=0).dims}] = mappings[index].sel(t=0)
-                si_utils.set_sim_affine(sim, affine_3d, transform_key=self.reg_transform_key, base_transform_key=self.source_transform_key)
-
             output_stack_properties = calc_output_properties(sims, self.reg_transform_key, z_scale=z_scale)
             # set z shape which is wrongly calculated by calc_stack_properties_from_view_properties_and_params
             # because it does not take into account the correct input z spacing because of stacks of one z plane

src/image/source_helper.py
@@ -22,6 +22,8 @@ def get_images_metadata(filenames):
     sizes = []
     centers = []
     rotations = []
+    positions = []
+    max_positions = []
     for filename in filenames:
         source = create_source(filename)
         size = source.get_physical_size_micrometer()
@@ -39,13 +41,24 @@ def get_images_metadata(filenames):
             size = list(size) + [0]
         center = np.array(position) + np.array(size) / 2
         centers.append(center)
+        positions.append(position)
+        max_positions.append(np.array(position) + np.array(size))
+    center = np.mean(centers, 0)
+    area = np.max(max_positions, 0) - np.min(positions, 0)
+    summary += f'Center: {tuple(center)} Area: {tuple(area)}\n'
 
     rotations2 = []
     for rotation, size in zip(rotations, sizes):
         if rotation is None:
             _, angles = get_orthogonal_pairs(centers, size)
-            rotation = -np.mean(angles)
-        rotations2.append(rotation)
-    return {'center': np.mean(centers, 0),
-            'rotation': np.mean(rotations2),
+            if len(angles) > 0:
+                rotation = -np.mean(angles)
+                rotations2.append(rotation)
+    if len(rotations2) > 0:
+        rotation = np.mean(rotations2)
+    else:
+        rotation = None
+    return {'center': center,
+            'area': area,
+            'rotation': rotation,
             'summary': summary}

src/util.py
@@ -309,19 +309,14 @@ def normalise_rotated_positions(positions0, rotations0, size, center):
     # in [xy(z)]
     positions = []
     rotations = []
-    pairs, angles = get_orthogonal_pairs(positions0, size)
-    if len(pairs) > 0:
-        mean_angle = np.mean(angles)
-        for position0, rotation in zip(positions0, rotations0):
-            if rotation is None:
-                rotation = -mean_angle
-            transform = create_transform(center=center, angle=-rotation, matrix_size=4)
-            position = apply_transform([position0], transform)[0]
-            positions.append(position)
-            rotations.append(rotation)
-    else:
-        positions = positions0
-        rotations = rotations0
+    _, angles = get_orthogonal_pairs(positions0, size)
+    for position0, rotation in zip(positions0, rotations0):
+        if rotation is None and len(angles) > 0:
+            rotation = -np.mean(angles)
+        transform = create_transform(center=center, angle=-rotation, matrix_size=4)
+        position = apply_transform([position0], transform)[0]
+        positions.append(position)
+        rotations.append(rotation)
     return positions, rotations
 
 

test/ome_testing.py
@@ -203,9 +203,10 @@ def test5(sims, tmp_path):
     transform_key = 'stage_metadata'
     new_transform_key = 'registered'
 
-    from src.registration_methods.RegistrationMethodCPD import RegistrationMethodCPD
-    registration_method = RegistrationMethodCPD(sims[0].dtype)
-    pairwise_reg_func = registration_method.registration
+    #from src.registration_methods.RegistrationMethodCPD import RegistrationMethodCPD
+    #registration_method = RegistrationMethodCPD(sims[0].dtype)
+    #pairwise_reg_func = registration_method.registration
+    pairwise_reg_func = registration.phase_correlation_registration
 
     # register in 2D
     # pairs: pairwise consecutive views
@@ -222,7 +223,8 @@ def test5(sims, tmp_path):
     )
     progress.update()
     progress.close()
-    print(params)
+    for param in params:
+        print(param.data[0].tolist())
 
     # set 3D affine transforms from 2D registration params
     for index, sim in enumerate(sims):
@@ -232,6 +234,12 @@ def test5(sims, tmp_path):
 
     # continue with new transform key
     transform_key = new_transform_key
+
+    print(f'New transforms shape: {sims[0].transforms[new_transform_key].shape}')
+    progress = tqdm(desc='Plot', total=1)
+    vis_utils.plot_positions([msi_utils.get_msim_from_sim(sim) for sim in sims], transform_key=transform_key, use_positional_colors=False)
+    progress.update()
+    progress.close()
 
     z_scale = 0.5
 
@@ -256,6 +264,9 @@ def test5(sims, tmp_path):
     # because it does not take into account the correct input z spacing because of stacks of one z plane
     output_stack_properties['shape']['z'] = len(sims)
 
+    data_size = np.prod(list(output_stack_properties['shape'].values())) * sims[0].dtype.itemsize
+    print(f'Fused size {print_hbytes(data_size)}')
+
     progress = tqdm(desc='Fuse', total=1)
     # fuse all sims together using simple average fusion
     fused_image = fusion.fuse(
@@ -265,11 +276,6 @@ def test5(sims, tmp_path):
         output_chunksize={'z': 1, 'y': 1024, 'x': 1024},
         fusion_func=fusion.simple_average_fusion,
     )
-    progress.update()
-    progress.close()
-
-    progress = tqdm(desc='Plot', total=1)
-    vis_utils.plot_positions(reg_msims, transform_key=new_transform_key, use_positional_colors=False)
     progress.update()
     progress.close()
 
@@ -381,5 +387,5 @@ if __name__ == '__main__':
 
         path = Path('D:/slides/test_stack/')
         #create_stack(path, n=100)
-        test_create_stack(path, n=100)
-        #test_pipeline(path)
+        #test_create_stack(path, n=100)
+        test_pipeline(path)